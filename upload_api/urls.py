4d8941c19baf057878decd83eca262e44658394d
Nihar Kashyap <nihar1kashyap7@gmail.com>
2022-03-29 17:41:06 +0530

    Kiba

---
+++ upload_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from .views_save_db import *
+# from .views_save_file import *
+from .views_save_seek import *
+
+
+urlpatterns = [
+    path('', index),
+    path('upload_partial/', upload_partial),
+    path('upload_complete/', upload_complete),
+    path('show_file/<int:id>', show_file)
+]
